dbdf16f8d69bd1aa0670c5f7040f69f4511fb88e
nickjokic <nickyskater@gmail.com>
2022-08-07 17:40:55 +0200

    [REFACTOR] Refactors main.dart and bloc file exports.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:blocs_at_scale/src/app.dart';
 import 'package:blocs_at_scale/src/blocs/util/custom_bloc_observer.dart';
+import 'package:blocs_at_scale/src/services/format_helper.dart';
+import 'package:blocs_at_scale/src/services/repositories/product_repository.dart';
 import 'package:blocs_at_scale/src/services/repositories/user_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 import 'src/blocs/index.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = CustomBlocObserver();
 
-  BlocOverrides.runZoned(
-    () {
-      final _providers = [
-        Provider(create: (context) => UserRepository()),
-        BlocProvider<UserBloc>(create: (context) => UserBloc(userRepository: context.read<UserRepository>())),
-      ];
+  final _providers = [
+    Provider(create: (context) => UserRepository()),
+    Provider(create: (context) => ProductRepository()),
+    BlocProvider<UserBlocV2>(
+      create: (context) => UserBlocV2(userRepository: context.read<UserRepository>()),
+    ),
+    BlocProvider<ProductBloc>(
+      create: (context) => ProductBloc(productRepository: context.read<ProductRepository>()),
+    ),
+    BlocProvider<CartBloc>(
+      create: (context) => CartBloc(),
+    ),
+    Provider(create: (context) => FormatHelper()),
+  ];
 
-      runApp(
-        MultiProvider(
-          providers: _providers,
-          child: const App(),
-        ),
-      );
-    },
-    blocObserver: CustomBlocObserver(),
+  runApp(
+    MultiProvider(
+      providers: _providers,
+      child: const App(),
+    ),
   );
 }
--- lib/src/blocs/index.dart
+++ lib/src/blocs/index.dart
@@ -1,4 +1,18 @@
+// User Bloc
 export 'user/user_bloc.dart';
+export 'user/user_bloc_event_handler.dart';
 export 'user/user_event.dart';
 export 'user/user_state.dart';
-export 'user/user_bloc_event_handler.dart';
+export 'user_v2/user_bloc_v2.dart';
+// Cart Bloc
+export 'cart/cart_bloc.dart';
+export 'cart/cart_bloc_event_handler.dart';
+export 'cart/cart_event.dart';
+export 'cart/cart_state.dart';
+// Product Bloc
+export 'product/product_bloc.dart';
+// Remote Data Bloc
+export 'util/remote_data_bloc/remote_data_bloc.dart';
+export 'util/remote_data_bloc/remote_data_event.dart';
+export 'util/remote_data_bloc/remote_data_event_handler.dart';
+export 'util/remote_data_bloc/remote_data_state.dart';
