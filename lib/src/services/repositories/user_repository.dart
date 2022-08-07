0ab778040b160c839203db5f68b609fba784d3c6
nickjokic <nickyskater@gmail.com>
2022-08-07 17:19:42 +0200

    [REFACTOR] Updates UserRepository.

--- lib/src/services/repositories/user_repository.dart
+++ lib/src/services/repositories/user_repository.dart
@@ -1,21 +1,19 @@
 import 'package:blocs_at_scale/src/models/user_model.dart';
-
-const Duration _kFetchDelay = Duration(milliseconds: 1500);
-const User _kTestUser = User(userId: 420794622712, userEmail: 'ulf@tester.code', username: 'Ulf');
+import 'package:blocs_at_scale/src/util/demo_data/demo_data.dart';
+import 'package:blocs_at_scale/src/util/constants/util_constants.dart';
 
 /// Repository responsible for handling [User]-related fetches.
 class UserRepository {
-  final User _currentUser;
-  UserRepository() : _currentUser = _kTestUser;
+  UserRepository();
 
   /// Simulates the fetch and returns a [User] object.
   Future<User> fetchUser({bool simulateError = false}) async {
-    await Future.delayed(_kFetchDelay); // Simulate the response time
+    await Future.delayed(UtilConstants.kFetchDelay); // Simulate the response time
 
     if (simulateError) {
       throw Exception('An error occured during the fetching of the user.');
     }
 
-    return _currentUser;
+    return DemoData.kTestUser;
   }
 }
